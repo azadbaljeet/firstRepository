99e93d2d1dfeba7bda7e2435f0a4707ac608bcfc
apple <apple@apples-Mac-mini-4.local>
2015-11-04 11:23:29 +0530

    change in viewController

--- repository/ViewController.m
+++ repository/ViewController.m
@@ -16,6 +16,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UILabel   *label    =   [[UILabel alloc] initWithFrame:CGRectMake(20, 150, 300, 40)];
+    label.backgroundColor   =   [UIColor redColor];
+    [label setText:@"india jiodfig ,jof s,iofgfs f,dsiogfgfo"];
+    [self.view addSubview:label];
+    
+    UILabel   *label1    =   [[UILabel alloc] initWithFrame:CGRectMake(20, 200, 300, 40)];
+    label1.backgroundColor   =   [UIColor redColor];
+    [label1 setText:@"india is Great Country"];
+    [self.view addSubview:label1];
+    
+    UILabel   *label3    =   [[UILabel alloc] initWithFrame:CGRectMake(20, 260, 300, 40)];
+    label3.backgroundColor   =   [UIColor redColor];
+    [label3 setText:@"india is Great and large Country"];
+    [self.view addSubview:label3];
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
